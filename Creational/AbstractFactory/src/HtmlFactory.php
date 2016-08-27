5fc48c377506a113216774a66087a68f5eaf03c1
LA <ladams1776@yahoo.com>
2016-08-27 08:42:06 -0400

    Begining MediaInterface

--- Creational/AbstractFactory/src/HtmlFactory.php
+++ Creational/AbstractFactory/src/HtmlFactory.php
@@ -1,48 +1,39 @@
 <?php
-
+namespace Creational\AbstractFactory;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-namespace Creational\AbstractFactory;
-
 /**
- * Class JsonFactory
- * 
- * JsonFactory is a factory for creating a family of JSON component
- * (example for ajax).
+ * Description of HtmlFactory
  *
- * @author theAdmin
+ * @author the_admin
  */
-class JsonFactory extends AbstractFactory
+class HtmlFactory extends AbstractFactory
 {
     /**
      * Creates a picture component.
      * 
      * @param string $path
      * @param string $name
-     * 
-     * @return Json\Picture|Picture
+     * @return Html\Picture|Picture
      */
     public function createPicture($path, $name = '') 
     {
-        return new Json\Picture($path, $name);
+        return new Html\Picture($path, $name);
     }
+    
     /**
      * Creates a text component.
      * 
-     * 
      * @param string $content
-     * @return \Creational\AbstractFactory\Json\Text
+     * @return Html\Text|Text
      */
-    public function createText($content)
+    public function createText($content) 
     {
-        return new Json\Text($content);
+        return new Html\Text($content);
     }
-    
-    
-    
-    
+
 }
---
+++ Creational/AbstractFactory/src/MediaInterface.php
@@ -0,0 +1,12 @@
+<?php
+namespace Creational\AbstractFactory;
+
+/**
+ * Description of MediaInterface
+ *
+ * @author the_admin
+ */
+class MediaInterface 
+{
+
+}

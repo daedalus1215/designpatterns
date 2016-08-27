dfda12e3d502017f07ea5d4d9eec69df399405c7
LA <ladams1776@yahoo.com>
2016-08-27 08:52:01 -0400

    Beginning the actual Picture

---
+++ Creational/AbstractFactory/src/Json/Picture.php
@@ -0,0 +1,13 @@
+<?php
+namespace Creational\AbstractFactory\Json;
+
+use Creational\AbstractFactory\Picture as BasePicture;
+
+/**
+ * Description of Picture
+ *
+ * @author the_admin
+ */
+class Picture {
+    //put your code here
+}

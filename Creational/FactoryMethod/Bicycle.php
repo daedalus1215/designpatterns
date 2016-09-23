06f43777325525f4ff6bd0a6d2de64baf38fe8d4
larry <ladams1776@yahoo.com>
2016-09-22 20:55:30 -0400

    Bicycle

---
+++ Creational/FactoryMethod/Bicycle.php
@@ -0,0 +1,18 @@
+<?php
+namespace Creational\FactoryMethod;
+
+
+
+class Bicycle implements VehicleInterface
+{
+    /**
+     *
+     * @var string 
+     */
+    protected $color;
+    
+    public function setColor($rgb) 
+    {
+        $this->color = $rgb;
+    }
+}

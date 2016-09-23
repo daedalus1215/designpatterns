42eb679316d305a872456614c5c57a1e50ce8e27
larry <ladams1776@yahoo.com>
2016-09-22 21:27:10 -0400

    completed FactoryMethodTest

---
+++ Creational/FactoryMethod/test/FactoryMethodTest.php
@@ -0,0 +1,49 @@
+<?php
+namespace Creational\FactoryMethod\test;
+
+
+require_once('vendor\autoload.php');
+/**
+ * Description of FactoryMethodTest
+ *
+ */
+class FactoryMethodTest extends \PHPUnit\Framework\TestCase
+{
+    protected $types = array(
+        FactoryMethod::CHEAP,
+        FactoryMethod::FAST        
+    );
+    
+    
+    public function getShop()
+    {
+        return array(
+            array(new \Creational\FactoryMethod\Factory\GermanFactory()),
+            array(new \Creational\FactoryMethod\Factory\ItalianFactory())
+        );
+    }
+              
+    /**
+     * 
+     * @dataProvider getShop
+     */
+    public function testCreation(FactoryMethod $shop)
+    {
+        // this test method acts as a client for the factory. We don't care
+        // about the factory, all we know is it can produce a vehicle.
+        foreach ($this->types as $type) {
+            $vehicle = $shop->create($type);
+            $this->assertInstanceOf('Creational\FactoryMethod\VehicleInterface', $vehicle);
+        }        
+    }
+    
+    /**
+     * @dataProvider getShop
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage spaceship is not a valid vehicle
+     */
+    public function testUnknownType(FactoryMethod $shop)
+    {
+        $shop->create('spaceship');
+    }    
+}
